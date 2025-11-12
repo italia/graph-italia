0aaeef09ed37e65b053d9849a8d240748f620819
Valerio Como <v.como@innovazione.gov.it>
2025-11-12 11:00:03 +0000

    chore: setup postCreateCommand script

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+echo "Post Create Command"
+echo "Post Create Command: Installing dependencies with Bun"
+bun i
+
+echo "Post Create Command: Installing Prisma CLI globally"
+bun i -g prisma@5.19.0
+
+
+
