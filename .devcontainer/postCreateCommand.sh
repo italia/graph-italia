767efee44f2633db10a465468246dc1efc57e5ed
Valerio Como <v.como@innovazione.gov.it>
2025-12-19 16:10:57 +0000

    fix: devcontainer automation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,5 +5,11 @@ bun i
 echo "Post Create Command: Installing Prisma CLI globally"
 bun i -g prisma@latest
 
+if [ ! -e packages/server/.env ]; then
+    cp packages/server/sample.env packages/server/.env
+fi
 
+if [ ! -e packages/webapp/.env ]; then
+    cp packages/webapp/sample.env packages/webapp/.env
+fi
 
