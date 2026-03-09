a8ef4f72b83b8fb9d7f8ffc42f2528bf82ddf3ba
Lorezz <point.point@gmail.com>
2026-03-09 12:35:36 +0100

    fixing A11y stuff, added lib

---
+++ packages/webapp/src/components/EditStepComponent.tsx
@@ -0,0 +1,35 @@
+export default function EditStepComponent(props: {
+  title: string;
+  description?: string;
+  Icon: React.ElementType;
+  children: React.ReactNode;
+  isOpen: boolean;
+  isDisabled: boolean;
+  index: number;
+}) {
+  const { title, description, Icon, children, isOpen, index, isDisabled } = props;
+  return (
+    <details className="collapse collapse-arrow bg-base-100 border border-base-300" name={`my-accordion-det-${index}`} aria-disabled={isDisabled} open={isOpen}>
+      <summary className="collapse-title font-semibold">
+        <div className="flex items-center gap-3 mb-4">
+          <div className="w-10 h-10 rounded-full bg-primary/10 flex items-center justify-center">
+            <span className="text-primary font-bold">
+              <Icon />
+            </span>
+          </div>
+          <div>
+            <h2 className="card-title text-xl">
+              {title}
+            </h2>
+            <p className="text-sm text-base-content/60">
+              {description || ""}
+            </p>
+          </div>
+        </div>
+      </summary>
+      <div className="collapse-content text-sm">
+        {children}
+      </div>
+    </details>
+  );
+}
---
+++ packages/webapp/src/components/ThemeSwitcherComponent.tsx
@@ -0,0 +1,24 @@
+export default function ThemeSwitcherComponent(props: {
+  currentTheme: "light" | "dark";
+  handleChange: (value: string) => void;
+}) {
+  const { currentTheme, handleChange } = props;
+  return (
+    <div className="flex justify-end mb-2">
+      <label className="flex items-center gap-2 cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" title="light" alt="light">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+        <input
+          type="checkbox"
+          className="toggle toggle-sm"
+          checked={currentTheme === "dark"}
+          onChange={(e) => handleChange(e.target.checked ? "dark" : "light")}
+        />
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" title="dark" alt="dark">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        </svg>
+      </label>
+    </div>
+  );
+}
