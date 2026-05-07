9c500c2310c5c27fccc3e3d78992602aba9ce736
Lorezz <point.point@gmail.com>
2026-05-07 10:37:19 +0200

    fix recover form

--- packages/webapp/src/components/auth/ResetPasswordForm.tsx
+++ packages/webapp/src/components/auth/ResetPasswordForm.tsx
@@ -45,6 +45,7 @@ function ResetPasswordForm({
   );
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const schema = getResetPasswordSchema(zod, t);
   const {
     register,
@@ -105,12 +106,40 @@ function ResetPasswordForm({
                 {t(`form.fields.password.label`)}
               </label>
               <div className="mt-2">
-                <input
-                  id="reset-password"
-                  type="password"
-                  className="input input-bordered w-full"
-                  {...register("password")}
-                />
+                <div className="relative">
+                  <input
+                    id="reset-password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter a new password"
+                    className="input input-bordered w-full"
+                    {...register("password")}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-content rounded-e-md focus:text-primary"
+                  >
+                    <svg
+                      className="shrink-0 size-3.5"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
+                      <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
+                      <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+                      <line className={showPassword ? "hidden" : "block"} x1="2" x2="22" y1="2" y2="22" />
+                      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+                      <circle cx="12" cy="12" r="3" />
+                    </svg>
+                  </button>
+                </div>
                 {errors["password"] && (
                   <p className="text-error">{errors["password"].message}</p>
                 )}
