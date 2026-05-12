7bd5547fb8513b188b91dd360184aed9d16093e0
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2026-05-12 14:32:00 +0200

    feat(accessibility): add accesibility changes

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -16,7 +16,7 @@ export type { DashboardProviderProps } from "./context/DashboardProvider";
 export type { ChartColorScheme, EchartsThemeValue } from "./themes";
 export type { ChartProviderProps } from "./context/ChartProvider";
 export type { DashboardProviderProps } from "./context/DashboardProvider";
-export type { DashboardProviderGridProps } from "./context/DashboardGridProvider";
+export type { DashboardGridProviderProps } from "./context/DashboardGridProvider";
 export type { DashboardData, DashboardSlot, RenderDashboardProps } from "./components/RenderDashboard";
 
 export * from "./types";
