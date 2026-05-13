6d16b503fcff9898c4c79b87255c4314fc3445f8
Valerio <valeriocomo@users.noreply.github.com>
2026-05-13 11:58:24 +0200

    chore: added ISSUE_TEMPLATE folder (#52)
    * fix(webapp) error and success message on recover password
    
    * removed todo
    
    * feat:(admin) added admin panel
    
    * feat: improved user dashboard
    
    * added change password and fix some style issues
    
    * rewritten readme
    
    * fix readme
    
    * fix prisma models mermaid
    
    * fix landingpage
    
    * feat chart and dashboard providers inside library, bumbed version, updated example app and sample code on landing page
    
    * fix text
    
    * feat(accessibility): add accesibility changes
    
    * feat: add issue templates for reporting bugs and discussions in Italian and English
    
    ---------
    
    Co-authored-by: Lorezz <point.point@gmail.com>
    Co-authored-by: Alessandro Palazzesi <alessandro.palazzesi@gmail.com>

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,31 +1,29 @@
-import ChartProvider from "./context/ChartProvider";
 import ChartWrapper from "./components/chartwrapper/ChartWrapper";
-import DashboardProvider from "./context/DashboardProvider";
-import DashboardGridProvider from "./context/DashboardGridProvider";
 import DataTable from "./components/dataTable/DataTable";
 import KpiItem from "./components/kpi/KpiItem";
 import PoweredBy from "./components/PoweredBy";
 import RenderChart from "./components/RenderChart";
 import RenderDashboard from "./components/RenderDashboard";
+import ChartProvider from "./context/ChartProvider";
 import { ColorSchemeProvider, useColorScheme, useResolvedTheme } from "./context/ColorSchemeContext";
-export type { ChartColorScheme, EchartsThemeValue } from "./themes";
-export type { ChartProviderProps } from "./context/ChartProvider";
-export type { DashboardProviderProps } from "./context/DashboardProvider";
-export type { DashboardProviderGridProps } from "./context/DashboardGridProvider";
+import DashboardGridProvider from "./context/DashboardGridProvider";
+import DashboardProvider from "./context/DashboardProvider";
 export type { DashboardData, DashboardSlot, RenderDashboardProps } from "./components/RenderDashboard";
+export type { ChartProviderProps } from "./context/ChartProvider";
+export type { DashboardGridProviderProps } from "./context/DashboardGridProvider";
+export type { DashboardProviderProps } from "./context/DashboardProvider";
+export type { ChartColorScheme, EchartsThemeValue } from "./themes";
 
 export * from "./types";
 export {
   ChartProvider,
   ChartWrapper,
-  ColorSchemeProvider,
-  DashboardProvider,
-  DashboardGridProvider,
-  DataTable,
+  ColorSchemeProvider, DashboardGridProvider, DashboardProvider, DataTable,
   KpiItem,
   PoweredBy,
   RenderChart,
   RenderDashboard,
   useColorScheme,
-  useResolvedTheme,
+  useResolvedTheme
 };
+
